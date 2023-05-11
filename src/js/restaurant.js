8d62f207a6a8f604e24b28a35bcc4c0839bde5c9
Kushnierova <Katerynakushnierova@gmail.com>
2023-05-11 21:59:40 +0200

    21:59
    Поки ще не пушити!!!!!!!

---
+++ src/js/restaurant.js
@@ -0,0 +1,29 @@
+const gallery = document.querySelector(".slider-room");
+
+document.addEventListener("click", (e) => {
+  e.preventDefault();
+  // console.log(e);
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src = ${e.target.dataset} width="800" height="600">
+`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", closeByEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", closeByEsc);
+      },
+    }
+  );
+  instance.show();
+
+  function closeByEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+});
