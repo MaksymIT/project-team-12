8cbb8817ec403ccc578078e89d6780c12235c81d
MaksymIT <maksymkibisg@gmail.com>
2024-05-24 16:02:19 +0200

    Final change

--- src/js/modal-room.js
+++ src/js/modal-room.js
@@ -13,3 +13,22 @@
   }
   
 })();
+
+ //validace formy
+
+const inputName = document.querySelector(".modal-input");
+
+const nameInput = (event) => {
+
+  event.preventDefault();
+
+  const inputName = event.currentTarget.elements.user_name.value;
+
+  if (!/^[A-ZĚŠČŘŽÝÁÍÉŮÚŤŇÓ][a-zěščřžýáíéůúťňó]+$/.test(inputName)) {
+    console.log("Jméno musí začínat velkým písmenem a obsahovat pouze písmena.");
+    return;
+  }
+}
+
+inputName.addEventListener("focus", nameInput);
+
